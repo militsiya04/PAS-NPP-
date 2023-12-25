28322b1c1c94a0d54e1fc2c846e723d54a321e7f
Denis <>
2023-12-25 06:52:10 +0200

    class worker
    created class worker

---
+++ NPP/src/Entities/Worker.java
@@ -0,0 +1,53 @@
+package Entities;
+
+public class Worker {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    private Worker(int id, String firstName, String lastName, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public static Worker createWorker(int id, String firstName, String lastName, String password) {
+        return new Worker(id, firstName, lastName, password);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
