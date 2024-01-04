56b146858425a153707c8c30acbc1f2826b280b8
Denis <>
2024-01-04 17:17:10 +0200

    modificated tests
    modificated tests

--- NPP/src/tests/repositories/ApartmentRepositoryTest.java
+++ NPP/src/tests/repositories/ApartmentRepositoryTest.java
@@ -1,5 +1,6 @@
 package tests.repositories;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 import Entities.Apartment;
 import Entities.Building;
 import Entities.Street;
@@ -9,43 +10,137 @@ import Repositories.Impl.BaseRepository;
 import java.util.List;
 
 public class ApartmentRepositoryTest {
-    public static void main(String[] args) {
-        IRepository<Apartment> ApartmentRepository = new BaseRepository<Apartment>() {
+    @Test
+    public void testCreate() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
             @Override
             protected int getItemId(Apartment item) {
                 return item.getId();
             }
         };
+
         Street street = new Street(12, "Test");
         Building building = new Building(13, 2);
-        Street street2 = new Street(14, "Test2");
-        Building building2 = new Building(15, 6);
 
-        //create
-        ApartmentRepository.create(new Apartment(1,23, street, building));
-        ApartmentRepository.create(new Apartment(2, 24, street2, building2));
+        // Create
+        apartmentRepository.create(new Apartment(1, 23, street, building));
+        apartmentRepository.create(new Apartment(2, 24, street, building));
 
-        //getAll
-        List<Apartment> allApartments = ApartmentRepository.getAll();
+        List<Apartment> allApartments = apartmentRepository.getAll();
+        System.out.println("All Apartments after create: " + allApartments);
+
+        assertEquals(2, allApartments.size());
+    }
+
+    @Test
+    public void testGetAll() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        apartmentRepository.create(new Apartment(1, 23, street, building));
+        apartmentRepository.create(new Apartment(2, 24, street, building));
+
+        // GetAll
+        List<Apartment> allApartments = apartmentRepository.getAll();
         System.out.println("All Apartments: " + allApartments);
 
-        //getbyid
-        Apartment apartmentById = ApartmentRepository.get(1);
-        System.out.println("Apartments by ID: " + apartmentById);
+        assertEquals(2, allApartments.size());
+    }
 
-        //find
-        List<Apartment> apartmentWithNameTest = ApartmentRepository.find(apartment -> apartment.getStreet().getName().equals("Test"));
+    @Test
+    public void testGetById() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        apartmentRepository.create(new Apartment(1, 23, street, building));
+        apartmentRepository.create(new Apartment(2, 24, street, building));
+
+        // GetById
+        Apartment apartmentById = apartmentRepository.get(1);
+        System.out.println("Apartment by ID: " + apartmentById);
+
+        assertNotNull(apartmentById);
+    }
+
+    @Test
+    public void testFind() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        apartmentRepository.create(new Apartment(1, 23, street, building));
+        apartmentRepository.create(new Apartment(2, 24, street, building));
+
+        // Find
+        List<Apartment> apartmentWithNameTest = apartmentRepository.find(apartment -> apartment.getStreet().getName().equals("Test"));
         System.out.println("Apartments with name Test: " + apartmentWithNameTest);
+
+        assertEquals(2, apartmentWithNameTest.size());
+    }
+
+    @Test
+    public void testUpdate() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        apartmentRepository.create(new Apartment(1, 23, street, building));
 
         // Update
         Apartment updatedApartment = new Apartment(1, 25, street, building);
-        ApartmentRepository.update(updatedApartment);
-        List<Apartment> allApartmentsAfterUpdate = ApartmentRepository.getAll();
+        apartmentRepository.update(updatedApartment);
+        List<Apartment> allApartmentsAfterUpdate = apartmentRepository.getAll();
         System.out.println("All Apartments after update: " + allApartmentsAfterUpdate);
 
-        //delete
-        ApartmentRepository.delete(1);
-        apartmentById = ApartmentRepository.get(1);
-        System.out.println("Apartments by ID: " + apartmentById);
+        assertEquals(1, allApartmentsAfterUpdate.size());
+        assertEquals(25, allApartmentsAfterUpdate.get(0).getNumber());
+    }
+
+    @Test
+    public void testDelete() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        apartmentRepository.create(new Apartment(1, 23, street, building));
+
+        // Delete
+        apartmentRepository.delete(1);
+        Apartment apartmentById = apartmentRepository.get(1);
+        System.out.println("Apartment by ID after delete: " + apartmentById);
+
+        assertNull(apartmentById);
     }
 }
--- NPP/src/Сatalog/getReport.java
+++ NPP/src/Сatalog/getReport.java
@@ -6,8 +6,8 @@ import Identities.Admin;
 import Identities.User;
 import Identities.Worker;
 import Repositories.IRepository;
-import Repositories.Impl.ApartmentRepository;
 import Repositories.Impl.BaseRepository;
+import org.junit.Test;
 
 public class getReport {
     public static String getReport(IRepository<Apartment> apartmentRepository, int id, User user) {
@@ -18,16 +18,13 @@ public class getReport {
             Furniture[] furnitureArray = apartment.getFurniture();
             Plumbing[] plumbingArray = apartment.getPlumbing();
 
-            // Заголовки таблицы
             result.append("FurnitureID\tFurnitureName\tPlumbingID\tPlumbingName\n");
 
-            // Заполнение строк таблицы
             for (int i = 0; i < Math.max(furnitureArray.length, plumbingArray.length); i++) {
                 if (i < furnitureArray.length) {
                     Furniture furniture = furnitureArray[i];
                     result.append(furniture.getId()).append("\t").append(furniture.getName()).append("\t");
                 } else {
-                    // Если элементов в массиве мебели меньше, добавляем пустые ячейки
                     result.append("\t\t");
                 }
 
@@ -45,38 +42,47 @@ public class getReport {
         }
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testGetReport() {
+        IRepository<Apartment> apartmentRepository = new BaseRepository<Apartment>() {
+            @Override
+            protected int getItemId(Apartment item) {
+                return item.getId();
+            }
+        };
+
+        Street street = new Street(12, "Test");
+        Building building = new Building(13, 2);
+
+        Furniture[] furnitureArray = {
+                new Furniture(1, "Table"),
+                new Furniture(2, "Chair"),
+                new Furniture(3, "Sofa"),
+                new Furniture(4, "Bed"),
+                new Furniture(5, "Bookshelf")
+        };
+
+        Plumbing[] plumbingArray = {
+                new Plumbing(1, "Sink"),
+                new Plumbing(2, "Toilet"),
+                new Plumbing(3, "Bathtub"),
+                new Plumbing(4, "Shower"),
+                new Plumbing(5, "Faucet")
+        };
+
+        Apartment testApartment = new Apartment(1, 12, street, building);
+        testApartment.setFurniture(furnitureArray);
+        testApartment.setPlumbing(plumbingArray);
+
+        apartmentRepository.create(testApartment);
+
         User user = new Admin(2, "Admin");
         User user2 = new Worker(3, "Worker");
-            IRepository<Apartment> ApartmentRepository = new BaseRepository<Apartment>() {
-                @Override
-                protected int getItemId(Apartment item) {
-                    return item.getId();
-                }
-            };
-            Street street = new Street(12, "Test");
-            Building building = new Building(13, 2);
 
-            Furniture[] furnitureArray = {
-                    new Furniture(1, "Table"),
-                    new Furniture(2, "Chair"),
-                    new Furniture(3, "Sofa"),
-                    new Furniture(4, "Bed"),
-                    new Furniture(5, "Bookshelf")
-            };
+        String reportForUser = getReport(apartmentRepository, 1, user);
+        String reportForUser2 = getReport(apartmentRepository, 1, user2);
 
-            Plumbing[] plumbingArray = {
-                    new Plumbing(1, "Sink"),
-                    new Plumbing(2, "Toilet"),
-                    new Plumbing(3, "Bathtub"),
-                    new Plumbing(4, "Shower"),
-                    new Plumbing(5, "Faucet")
-            };
-        Apartment test = new Apartment(1, 12, street, building);
-        test.setFurniture(furnitureArray);
-        test.setPlumbing(plumbingArray);
-        ApartmentRepository.create(test);
-        System.out.println(getReport(ApartmentRepository, 1, user));
-        System.out.println(getReport(ApartmentRepository, 1, user2));
+        System.out.println("Report for User:\n" + reportForUser);
+        System.out.println("Report for User2:\n" + reportForUser2);
     }
 }
